823e86a009811a58c5e90f36c13d5fd3526c0ebc
Psycho_Dyno <102162301+Psycho-dyno@users.noreply.github.com>
2024-06-09 02:18:46 +0000

    Operation done

---
+++ 1_basics/4_operation.js
@@ -0,0 +1,35 @@
+let value=3
+let negValue=-value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+
+let str1="hello"
+let str2=" Mukul"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+3+"2")
+
+console.log(true)
+console.log(+true)
+// console.log(true+) // will give an error
+console.log(false)
+console.log(+false)
+console.log(+"")
+console.log(+"")
+// js also support preincrement ans postincrement
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+console.log(++gameCounter)
+console.log(gameCounter++)
